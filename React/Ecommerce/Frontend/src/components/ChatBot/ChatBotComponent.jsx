709170f69e18eb90ccb20537d8be5a4a4f5ef31c
dev <devaharsar2004@gmail.com>
2026-07-07 17:45:49 +0530

    Added backend chatbot

--- React/Ecommerce/Frontend/src/components/ChatBot/ChatBotComponent.jsx
+++ React/Ecommerce/Frontend/src/components/ChatBot/ChatBotComponent.jsx
@@ -40,7 +40,7 @@ function ChatBotComponent() {
     setMessage("");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat", {
+      const response = await fetch(`${import.meta.env.VITE_CHATBOT_API}/chat`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
